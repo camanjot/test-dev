7df531fd0c21610b0d27aed1d8e0aec086ac3433
amanjot-kaur <amanjot.kaur@salesforce.com>
2021-12-05 16:37:11 +0530

    testimg sonar

--- lightning_utility.js
+++ lightning_utility.js
@@ -129,6 +129,7 @@ var callbacks = {};
         } else if (_result.message == 'Fail' || !_result.isSuccess) {
             return undefined;
         }
+        console.log('Hey');
     };
     /**
      * Todo: Prepare the toast object and return back to the calling JS class
